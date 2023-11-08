97c55e1634d44507692262c612a370bcbf1dc7ef
INDU <indu.82.it@gmail.com>
2023-11-08 12:57:53 +0530

    content removed

--- auto_gitrepo.sh
+++ auto_gitrepo.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 User_name="INDU-THAKUR"
-Repo="API_Testing"
-TOKEN="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"
-NEW_DESCRIPTION="integration of curl and git"
-API_URL="https://newsapi.org/v2/everything?q=tesla&from=2023-11-04to=2023-11-05&sortBy=publishedAt&apiKey=e782b949696948f28fca32a65958eb78"
-curl -X PATCH -H "Authenticatin: Token $TOKEN" -d "{\"discription\": \" $NEW_DESCRIPTION \"}" $API_URL 
+
