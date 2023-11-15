de69d7bd6ebda0b64078f1ba01fb5ce5cd29e5d9
INDU <indu.82.it@gmail.com>
2023-11-15 14:09:42 +0530

    files updated

--- auto_gitrepo.sh
+++ auto_gitrepo.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-#
 User_name="INDU-THAKUR"
 Repo="gitapi"
+path="https://github.com/INDU-THAKUR/gitapi/blob/main/auto_gitrepo.sh"
 TOKEN="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"
 NEW_DESCRIPTION="integration of curl and git"
-API_URL="https://newsapi.org/v2/everything?q=tesla&from=2023-11-04to=2023-11-05&sortBy=publishedAt&apiKey=e782b949696948f28fca32a65958eb78"
-curl -X PUT -H "Authenticatin: Token $TOKEN" -d "{\"discription\": \" $NEW_DESCRIPTION \"}" $API_URL 
+API_URL="https://api.github.com/repos/${User_name}/${Repo}"
+#curl -H "Authenticatin: Token $TOKEN""$API_URL" -o Indu.txt 
+curl -X PUT -H "Authenticatin: Token $TOKEN" "$API_URL" -o I.sh
+
