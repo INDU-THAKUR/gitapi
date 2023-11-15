ee38f67968cc7cc969dddcb6c27f046f455e40fe
INDU <indu.82.it@gmail.com>
2023-11-15 16:17:53 +0530

    new features added

--- auto_gitrepo.sh
+++ auto_gitrepo.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 User_name="INDU-THAKUR"
 Repo="gitapi"
-path="https://github.com/INDU-THAKUR/gitapi/blob/main/auto_gitrepo.sh"
+#path="https://github.com/INDU-THAKUR/gitapi/blob/main/auto_gitrepo.sh"
 TOKEN="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"
 NEW_DESCRIPTION="integration of curl and git"
 API_URL="https://api.github.com/repos/${User_name}/${Repo}"
+current_cont=$(curl -s -H "authentication:token ${TOKEN}" "${API_URL}"
+new_content=$(echo -n "New content" | base)
+current_SHA= $(echo "${current_cont}" jq -r '.sha')
+json="{\"messges\": \"updatefile\", \"content1\": \"${new_content}\", \"sha\": \"${current_SHA}\"}"
 #curl -H "Authenticatin: Token $TOKEN""$API_URL" -o Indu.txt 
-curl -X PUT -H "Authenticatin: Token $TOKEN" "$API_URL" -o I.sh
+curl -X PUT -H "Authenticatin: Token ${TOKEN}" -H "content-type: Application/json" -d "${json}" "${API_URL}" -o I.sh
 
